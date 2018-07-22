3aeb71588571e747a258894ec348ad054e24ee69
Tim Perelygin <sfforum@yandex.ru>
2018-07-22 12:06:42 +0300

    first commit

---
+++ views/site/index.php
@@ -0,0 +1,19 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Подсистема администрирования информационной сиситемы управления проектом</h1>
+
+        <p class="lead">ЗАО Спектр</p>
+
+        
+    </div>
+
+    
+    </div>
+</div>
